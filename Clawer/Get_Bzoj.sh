529fd0841351f0af0176ef2cb44d476a3f95c0cd
XZYQvQ <konnyakuxzy@outlook.com>
2018-10-30 11:05:20 +0800

    add 5441 to 5460

--- Clawer/Get_Bzoj.sh
+++ Clawer/Get_Bzoj.sh
@@ -7,7 +7,7 @@ path="https://www.lydsy.com/JudgeOnline/problem.php?id="
 
 while [ $pid -le $top ]; do
 	echo "------------Getting Problem:$pid------------"
-	wget -m -k -A="problem.php?id="$pid",problem.php?id="$pid".tmp,problem.php?id="$pid".tmp.html,png,jpg,gif,webp,bmp,pdf,rar,zip,7z,txt,doc,docx,ppt,pptx" --reject="logo.png" --load-cookies="./cookies.txt" $path$pid
+	wget -m -k -e robots=off -A="problem.php?id="$pid",problem.php?id="$pid".tmp,problem.php?id="$pid".tmp.html,png,jpg,gif,webp,bmp,pdf,rar,zip,7z,txt,doc,docx,ppt,pptx" --reject="logo.png" --load-cookies="./cookies.txt" $path$pid
 	mv "./www.lydsy.com/JudgeOnline/problem.php?id="$pid".tmp" "./www.lydsy.com/JudgeOnline/"$pid".html"
 	echo $pid"Done."
 	let pid=pid+1
